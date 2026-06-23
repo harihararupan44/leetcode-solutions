74a6a4b78a57ac859fd6981179291a354d9ecc13
harihararupan44 <harihararupan45@gmail.com>
2026-06-23 21:46:02 +0530

    Added 392 IsSubsequence

---
+++ Strings/392-IsSubsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        int i=0,j=0;
+        while(i<n&&j<m){
+            char ch=s.charAt(i);
+            char ch1=t.charAt(j);
+            if(ch==ch1){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i==s.length();
+    }
+}
